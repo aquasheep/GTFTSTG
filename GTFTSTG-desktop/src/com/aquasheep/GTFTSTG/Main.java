b803ded32e67660c3cc1ecb50b9902724c05e700
Aquasheep <kennepm0@sewanee.edu>
2012-09-22 07:55:30 -0500

    All projects setup (except Applet version)

---
+++ GTFTSTG-desktop/src/com/aquasheep/GTFTSTG/Main.java
@@ -0,0 +1,16 @@
+package com.aquasheep.GTFTSTG;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+public class Main {
+	public static void main(String[] args) {
+		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
+		cfg.title = "GTFTSTG";
+		cfg.useGL20 = false;
+		cfg.width = 480;
+		cfg.height = 320;
+		
+		new LwjglApplication(new GTFTSTG(), cfg);
+	}
+}
