7ec1cda0e31a2e91556bfad20c42517293fdb534
Aquasheep <kennepm0@sewanee.edu>
2012-09-22 09:55:21 -0500

    Added base Item class.

--- GTFTSTG/src/com/aquasheep/GTFTSTG/GTFTSTG.java
+++ GTFTSTG/src/com/aquasheep/GTFTSTG/GTFTSTG.java
@@ -4,7 +4,9 @@ import com.aquasheep.GTFTSTG.Screens.SplashScreen;
 import com.badlogic.gdx.Game;
 
 public class GTFTSTG extends Game {
+	
 	public static final String LOG = "GTFTSTG Log";
+	public Item[] items;
 	
 	@Override
 	public void create() {		
