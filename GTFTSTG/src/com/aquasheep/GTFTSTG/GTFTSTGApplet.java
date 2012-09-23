ab6432db6bfdfb6eb8ac060bfd1c2db9fad45923
Aquasheep <kennepm0@sewanee.edu>
2012-09-22 22:13:52 -0500

    Added applet version.

---
+++ GTFTSTG/src/com/aquasheep/GTFTSTG/GTFTSTGApplet.java
@@ -0,0 +1,14 @@
+package com.aquasheep.GTFTSTG;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplet;
+
+public class GTFTSTGApplet extends LwjglApplet {
+
+    private static final long serialVersionUID = 1L;
+
+    public GTFTSTGApplet()
+    {
+        super(new GTFTSTG(), false);
+    }
+    
+}
